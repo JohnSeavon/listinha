4d18d7d890478deda234df232a9d6f85ab29bef7
Joao Novaes <juaum.markux@gmail.com>
2023-02-08 20:51:41 -0300

    Create Project and set Color Schemes

---
+++ app/lib/src/app_widget.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:listinha/main.dart';
+
+import 'shared/themes/themes.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.light,
+      theme: lightTheme,
+      darkTheme: darkTheme,
+      home: const MyHomePage(title: 'LISTINHA'),
+    );
+  }
+}
---
+++ backend/bin/backend.dart
@@ -0,0 +1,8 @@
+import 'dart:io';
+
+import 'package:backend/backend.dart' as backend;
+
+void main(List<String> arguments) async{
+  print('Hello world: ${backend.calculate()}!');
+  stdin.readLineSync();
+}
