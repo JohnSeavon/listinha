24d2ef57a5aa547b62fba8d862cd3c42882e84fc
Joao Novaes <juaum.markux@gmail.com>
2023-02-12 23:45:21 -0300

    feat: implement state management

--- app/lib/src/app_module.dart
+++ app/lib/src/app_module.dart
@@ -1,12 +1,19 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:listinha/src/configuration/configuration_page.dart';
+import 'package:listinha/src/shared/stores/app_store.dart';
 
 import 'home/home_module.dart';
 
 class AppModule extends Module {
   @override
+  // TODO: implement binds
+  List<Bind> get binds => [
+        AutoBind.singleton(AppStore.new),
+      ];
+
+  @override
   List<ModularRoute> get routes => [
-    ModuleRoute('/home', module: HomeModule()),
-    ChildRoute('/config', child: (context, args) => const ConfigurationPage())
-  ];
+        ModuleRoute('/home', module: HomeModule()),
+        ChildRoute('/config', child: (context, args) => const ConfigurationPage()),
+      ];
 }
